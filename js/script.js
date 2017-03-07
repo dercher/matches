98d366254e3f873df9418d90a00772c6f5e558c3
Iurii Voitenko <jurgeban2@yandex.ua>
2017-03-07 17:21:28 +0200

    many independent matches created

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 // document.createElement();getElementById();getElementsByTagName();getElementsByName();getElementsByClassName();querySelector();querySelectorAll();el.classList.add();el.innerHTML = "";e.style.background = '';el.appendChild();setInterval();setTimeout();clearInterval();
 setMatrix(4, 5);   
-
+createMatches(10);
 //=============== FUNCTIONS ============
 function setMatrix(num1, num2) {
   var wrapper = document.createElement('div');
@@ -15,39 +15,41 @@ function setMatrix(num1, num2) {
   wrapper.innerHTML = text;
   document.getElementsByTagName('body')[0].appendChild(wrapper);
 }
-//----------------------------
-function drag(el) {
-	var rect = el.getBoundingClientRect();
-  console.log(rect.top, rect.left);
-	el.style.top = '50px';
-	el.style.left ='50px';
-  rect = el.getBoundingClientRect();
-	console.log(rect.top, rect.left);
+//------------------------------------------
+function createMatches(num) {
+  var box = document.createElement('div');
+  box.classList.add('box');
+  var text = '', top = 0;
+  for (var i = 0; i < num; i++) {
+    text += '<div class="stock" onmousedown="moveMatch(event, this)" style="top: ' +(top+=20)+ 'px" ondblclick="rotateMatch(this)"></div>'
+  }
+  box.innerHTML = text;
+  document.getElementsByTagName('body')[0].appendChild(box);
 }
 //-------------------- MOUSEMOVE ---------
-  var elem = document.querySelector(".stock");
-  elem.addEventListener("mousedown", function(event) {
+ function moveMatch(event, elem) {
     if (event.which == 1) {
-      addEventListener("mousemove", moved);
+      addEventListener('mousemove', moved );
       event.preventDefault(); // Предотвращает выделение
     }
-  });
-//-----
+
+   function moved (event) {
+      if (!buttonPressed(event)) {  //-- удаляем событие чтоб можно было цеплять следующий элемент
+        removeEventListener('mousemove', moved);
+      } else {
+       elem.style.left = event.pageX - 50 + 'px';
+       elem.style.top = event.pageY - 42 + 'px';
+      }
+   }
+ }
+//--------
   function buttonPressed(event) {
     if (event.buttons == null)
       return event.which != 0;
     else
       return event.buttons != 0;
   }
- //-----
-  function moved(event) {
-    if (!buttonPressed(event)) {
-      removeEventListener("mousemove", moved);
-    } else {
-     elem.style.left =  event.pageX - 50 + 'px';
-     elem.style.top = event.pageY - 20 + 'px';
-    }
-  }
+
  //------------------------
  function rotateMatch(el) {
   var rotate = getComputedStyle(el);
@@ -55,3 +57,4 @@ function drag(el) {
   console.log('rotate', rotate)
   el.style.transform = 'matrix(1, 0, 0, 1, 0, 1)'
  }
+ //=============================
